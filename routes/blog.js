455bf688244ce9f65cae56f8fffc1d936af2bb4d
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-03-29 16:38:53 +0530

    using the blogs

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const path = require("path");
+const blogs = require("../data/blogs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../templates/index.html"));
+});
+
+router.get("/blog", (req, res) => {
+  blogs.forEach((element) => {
+    console.log(element.title);
+  });
 });
 
 module.exports = router;

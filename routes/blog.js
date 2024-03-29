9be3bbe2a507490acc513919316d03874d44583a
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-03-29 17:32:03 +0530

    made blogpages

--- routes/blog.js
+++ routes/blog.js
@@ -8,10 +8,17 @@ router.get("/", (req, res) => {
 });
 
 router.get("/blog", (req, res) => {
-  blogs.forEach((element) => {
-    console.log(element.content);
+  //   blogs.forEach((element) => {
+  //     console.log(element.content);
+  //   });
+  res.sendFile(path.join(__dirname, "../templates/blogHome.html"));
+});
+router.get("/blogpost/:slug", (req, res) => {
+  myBlog = blogs.filter((e) => {
+    return e.slug == req.params.slug;
   });
-  res.sendFile(_dirname, "../templates/blogHome.html");
+  
+  res.sendFile(path.join(__dirname, "../templates/blogpage.html"));
 });
 
 module.exports = router;

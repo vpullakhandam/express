09f89b0fc2df3f7d386b2ebd18ff6d595848f6ad
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-03-29 16:16:59 +0530

    serving the static folder in index.js

--- index.js
+++ index.js
@@ -3,17 +3,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-// how to use variables while sending request. the name should be replaced with a real name
-app.get("/hello/:name", (req, res) => {
-  res.send("hello world!" + req.params.name);
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "templates/index.html"));
-});
+app.use(express.static(path.join(__dirname,"static")))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);

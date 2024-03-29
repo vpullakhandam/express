9514f657c5d283c3b4e660207663d0eecca1a127
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-03-29 16:37:46 +0530

    added sample blogs

---
+++ data/blogs.js
@@ -0,0 +1,20 @@
+blogs: [
+  {
+    title: "A",
+    content: "content A",
+  },
+  {
+    title: "B",
+    content: "content B",
+  },
+  {
+    title: "C",
+    content: "content C",
+  },
+  {
+    title: "D",
+    content: "content D",
+  },
+];
+
+module.exports = blogs;

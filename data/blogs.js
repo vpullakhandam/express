464cc3acb9d55faa385f7479fa5d5f08032141b0
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-03-29 16:51:20 +0530

    added slugs to the sample blogs

--- data/blogs.js
+++ data/blogs.js
@@ -1,19 +1,23 @@
-blogs: [
+blogs = [
   {
     title: "A",
     content: "content A",
+    slug: "content-A-slug",
   },
   {
     title: "B",
     content: "content B",
+    slug:"content-B-slug",
   },
   {
     title: "C",
     content: "content C",
+    slug:"content-C-slug",
   },
   {
     title: "D",
     content: "content D",
+    slug:"content-D-slug",
   },
 ];
 
